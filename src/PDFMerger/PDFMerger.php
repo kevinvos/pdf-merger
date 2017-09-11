dfb28248f5cf5cb4d7ae1f19d3fb57f7e0f95362
kevinvos <k.vos@webdevelop.nl>
2017-09-11 10:51:37 +0200

    Update PDFMerger.php

--- src/PDFMerger/PDFMerger.php
+++ src/PDFMerger/PDFMerger.php
@@ -21,7 +21,7 @@
  * such as form fields, links or page annotations (anything not a part of the page content stream).
  *
  */
-namespace Clegginabox\PDFMerger;
+namespace PDFMerger;
 
 use Exception;
 use fpdi\FPDI;
